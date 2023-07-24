88d5dc04c459dd867ba32438fb5cb34b02ea1726
Samir Abdallah <samir.abdallah@telecomnancy.eu>
2023-07-24 23:39:36 +0200

    implémentation test_get_page_content

--- test/aggregator_test.py
+++ test/aggregator_test.py
@@ -1,9 +1,12 @@
 import sys
 sys.path.insert(0,'./')
 from src.aggregator import get_page_content
+from selenium import webdriver
 
 class TestAggregator:
     def test_get_page_content(self):
-        url:str = "https://www.oddsportal.com/football/france/ligue-1-2022-2023/results/"
-        content = get_page_content(url)
-        assert (content > 0)
+            options= webdriver.ChromeOptions()
+            options.add_argument("--headless")
+            driver = webdriver.Chrome(options=options)
+            r = get_page_content(driver=driver,URL="https://www.oddsportal.com/football/france/ligue-1-2022-2023/results/")
+            assert len(r) == 30
